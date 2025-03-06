b33c14162cacc1469156bccc383f5b854e0bbd5b
Nicola31082005 <nikule512@gmail.com>
2025-03-06 18:30:07 +0200

    Add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { Route, Routes } from 'react-router'
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Home from './components/Home'
 
 function App() {
-
   return (
-    <>
+    <div className="pt-16"> {/* Add padding-top to avoid header overlap */}
+      <Header />
 
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <Footer />
+    </div>
   )
 }
 
