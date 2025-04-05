3e761f360dfc0643ec0e2426e6433c826786fccd
Nicola31082005 <nikule512@gmail.com>
2025-04-05 22:37:20 +0300

    fix applicant pictures sizes

--- frontend/src/components/common/JobApplicantListItem.jsx
+++ frontend/src/components/common/JobApplicantListItem.jsx
@@ -7,14 +7,22 @@ export default function JobApplicantListItem({
     first_name,
     last_name,
     avatar,
-
 }) {
     return (
-        <Link to={`/jobs/${id}`} key={id} data-testid={`job-applicant-list-item`} className="bg-white p-6 shadow-md rounded-lg cursor-pointer">
+        <Link
+            to={`/jobs/${id}`}
+            key={id}
+            data-testid={`job-applicant-list-item`}
+            className="bg-white p-6 shadow-md rounded-lg cursor-pointer"
+        >
             <h3 className="text-xl font-semibold text-blue-600">{email}</h3>
             <p className="text-gray-700">{first_name}</p>
             <p className="text-gray-500">{last_name}</p>
-            <img src={avatar} alt={first_name} />
+            <img
+                src={avatar}
+                alt={first_name}
+                className="w-32 h-32 object-cover mt-2"
+            />
             <div className="mt-4">
                 <button
                     to={`/jobs/${id}`}
